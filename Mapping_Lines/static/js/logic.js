b519d1f88a3679301f3a71674778785fc522773d
mleegan89 <mleegan89@gmail.com>
2022-04-03 13:55:36 -0400

    recommit

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -8,11 +8,11 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 let line = [
     [33.9416, -118.4085],
     [37.6213, -122.3790],
-    [40.7899, -111.9791],
+    [40.7899, -111.9791], 
     [47.4502, -122.3088]
   ];
 
-// Create a polyline using the line coordinates and make the line yellow.
+// Create a polyline using the line coordinates and make the line yellow. Recommit 
 L.polyline(line, {
     color: "yellow"
  }).addTo(map);
